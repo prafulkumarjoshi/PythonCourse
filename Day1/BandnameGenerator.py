1fe47e2b3a63b535faf22394305ac7c0c1b82441
Prafulkumar Joshi <prafulkumar_joshi@hotmail.com>
2025-12-27 20:21:58 +0530

    Day python course

---
+++ Day1/BandnameGenerator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band name Generator!")
+
+home_city = input("What's the name of the city you grew up in?\n")
+
+
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name could be " + home_city + " " + pet_name)
+
+
--- Day1/task1.py
+++ Day1/task1.py
@@ -1,2 +1,11 @@
 print("Hello World!" + " " + "Hello World!")
 
+#Take a input from user and print Hello to the user
+#print("Hello " + input("What is your name? \n") + "!")
+
+#get user name as input and print number of characters in it
+name = input("What is your name?")
+print(len(name))
+
+
+
